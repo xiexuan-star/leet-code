bb0d583653a67e44c43cb10bc85793c773aaaa82
xiexuan-star <xiexuan127@foxmail.com>
2023-01-30 14:37:42 +0800

    feat: 迭代法遍历二叉树

--- learning/1-二叉树的遍历.js
+++ learning/1-二叉树的遍历.js
@@ -1,3 +1,10 @@
+/**
+ * @typedef {{val:string,left?:TreeNode,right?:TreeNode}} TreeNode
+ */
+
+/**
+ * @type {TreeNode}
+ */
 const root = {
   val: "A",
   left: {
@@ -17,48 +24,136 @@ const root = {
   }
 };
 
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-// 先序遍历
-function preorderTraversal(root, result = []) {
-  if (!root) return [];
-  result.push(root.val);
+{
+  console.log("// ------------------------------------ 递归 ---------------------------------");
+  /**
+   * @param {TreeNode} root
+   * @param {string[]} result
+   * @returns {string[]}
+   */
+  function preorderTraversal(root, result = []) {
+    if (!root) return [];
+    result.push(root.val);
 
-  preorderTraversal(root.left, result);
-  preorderTraversal(root.right, result);
+    preorderTraversal(root.left, result);
+    preorderTraversal(root.right, result);
 
-  return result;
+    return result;
+  }
+
+  console.log("preorder_traversal=>", preorderTraversal(root));
+
+  /**
+   * @param {TreeNode} root
+   * @param {string[]} result
+   * @returns {string[]}
+   */
+  function inorderTraversal(root, result = []) {
+    if (!root) return [];
+
+    inorderTraversal(root.left, result);
+    result.push(root.val);
+    inorderTraversal(root.right, result);
+
+    return result;
+  }
+
+  console.log("inorder_traversal=>", inorderTraversal(root));
+
+  /**
+   * @param {TreeNode} root
+   * @param {string[]} result
+   * @returns {string[]}
+   */
+  function postorderTraversal(root, result = []) {
+    if (!root) return [];
+
+    postorderTraversal(root.left, result);
+    postorderTraversal(root.right, result);
+
+    result.push(root.val);
+
+    return result;
+  }
+
+  console.log("postorder_traversal=>", postorderTraversal(root));
 }
 
-console.log(preorderTraversal(root));
+{
+  console.log("// ------------------------------------ 迭代 ---------------------------------");
 
-function inorderTraversal(root, result = []) {
-  if (!root) return [];
+  /**
+   * @param {TreeNode} tree
+   * @returns {string[]}
+   */
+  function preorderTraversal(tree) {
+    /**
+     * @type {string[]}
+     */
+    const res = [];
 
-  inorderTraversal(root.left, result);
-  result.push(root.val);
-  inorderTraversal(root.right, result);
+    /**
+     * @type {TreeNode[]}
+     */
+    const stack = [tree];
 
-  return result;
+    while (stack.length) {
+      const current = stack.pop();
+      res.push(current.val);
+      current.right && stack.push(current.right);
+      current.left && stack.push(current.left);
+    }
+    return res;
+  }
+
+  console.log("preorder_traversal=>", preorderTraversal(root));
+
+
+  /**
+   * @param {TreeNode} tree
+   * @returns {string[]}
+   */
+  function postorderTraversal(tree) {
+    /**
+     * @type {string[]}
+     */
+    const res = [];
+
+    /**
+     * @type {TreeNode[]}
+     */
+    const stack = [tree];
+
+    while (stack.length) {
+      const current = stack.pop();
+      res.unshift(current.val);
+      current.left && stack.push(current.left);
+      current.right && stack.push(current.right);
+    }
+    return res;
+  }
+
+  console.log("postorder_traversal=>", postorderTraversal(root));
+
+  /**
+   * @param {TreeNode} tree
+   * @returns {string[]}
+   */
+  function inorderTraversal(tree) {
+    const res = []
+    const stack = []
+    let cur = tree;
+    while(cur || stack.length) {
+      while(cur) {
+        stack.push(cur)
+        cur = cur.left
+      }
+      cur = stack.pop()
+      res.push(cur.val)
+      cur = cur.right
+    }
+    return res
+  }
+
+  console.log("inorder_traversal=>", inorderTraversal(root));
 }
-
-console.log(inorderTraversal(root));
-
-function postorderTraversal(root, result = []) {
-  if (!root) return [];
-
-  postorderTraversal(root.left, result);
-  postorderTraversal(root.right, result);
-
-  result.push(root.val);
-
-  return result;
-}
-
-console.log(postorderTraversal(root));
